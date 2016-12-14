04ec2002f542b2265681c9736307865c43cf0324
pangzhihao <boy.run.fast.china@gmail.com>
2016-12-14 18:51:22 +0800

    通读源码后发现51行bug,ie6/ie7下也需要补齐消息前缀才能正常工作，否则与第90行代码矛盾

--- messenger.js
+++ messenger.js
@@ -48,7 +48,7 @@ window.Messenger = (function(){
         Target.prototype.send = function(msg){
             var targetFunc = window.navigator[this.prefix + this.name];
             if ( typeof targetFunc == 'function' ) {
-                targetFunc(this.prefix + msg, window);
+                targetFunc(this.prefix + '|' + this.name + '__Messenger__' + msg, window);
             } else {
                 throw new Error("target callback function is not defined");
             }
@@ -79,7 +79,7 @@ window.Messenger = (function(){
             if(typeof msg == 'object' && msg.data){
                 msg = msg.data;
             }
-            
+
             var msgPairs = msg.split('__Messenger__');
             var msg = msgPairs[1];
             var pairs = msgPairs[0].split('|');
